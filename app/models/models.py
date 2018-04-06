b27833144168000e3b28e57651a5a42a7fabc529
Jamie <jamin131@gmail.com>
2018-04-06 18:45:10 -0400

    changed models config

--- app/models/models.py
+++ app/models/models.py
@@ -8,10 +8,10 @@ from app import db
 #
 class User(db.Model):
     __tablename__ = 'users'
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(120), unique=True)
-    email = db.Column(db.String(120), unique=True)
-    join_date = db.Column(db.String(120), unique=False)
+    id = db.Column(db.String(500), primary_key=True)
+    name = db.Column(db.String(500), unique=True)
+    email = db.Column(db.String(500), unique=True)
+    join_date = db.Column(db.String(500), unique=False)
     ref_code = db.Column(db.String(500), unique=True)
     ref_key = db.Column(db.String(500), unique=True)
 
@@ -26,10 +26,10 @@ class User(db.Model):
 class Track(db.Model):
     __tablename__ = 'tracks'
     id = db.Column(db.Integer, primary_key=True)
-    user_id = db.Column(db.Integer, unique=False)
-    track_id = db.Column(db.Integer, unique=True)
-    playlist_id = db.Column(db.String(500), unique=True)
-    date = db.Column(db.String(120), unique=False)
+    user_id = db.Column(db.String(500), unique=False)
+    track_id = db.Column(db.String(500), unique=True)
+    playlist_id = db.Column(db.String(500), unique=False)
+    date = db.Column(db.String(500), unique=False)
     track_key = db.Column(db.String(500), unique=True)
 
     def __init__(self, id, user_id, track_id, playlist_id, date, track_key):
@@ -42,8 +42,8 @@ class Track(db.Model):
 
 class Playlist(db.Model):
     __tablename__ = 'playlists'
-    user_id = db.Column(db.Integer, unique=False)
-    playlist_id = db.Column(db.String(500), unique=True)
+    user_id = db.Column(db.String(500), unique=False)
+    playlist_id = db.Column(db.String(500), unique=False)
     p_key = db.Column(db.String(500), primary_key=True, unique=True)
 
     def __init__(self, user_id, playlist_id, p_key):
